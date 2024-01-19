53ddc6745c0d0a09006de88261b94ac0e3aa8ca1
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-01-19 19:19:44 +0600

    add here new styles

--- App.tsx
+++ App.tsx
@@ -1,29 +1,24 @@
-import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {styled} from 'nativewind';
-const StyleView = styled(View);
-const StyleText = styled(Text);
+
 const App = () => {
-  const isDarkMood = useColorScheme() === 'dark';
   return (
-    <>
-      <StyleView className="bg-white flex-1 px-2 pt-5 flex justify-center items-center">
-        <StyleText className={`${isDarkMood ? 'text-white' : 'text-black'}`}>
-          App
-        </StyleText>
-        <Text style={styles.container}>App</Text>
-      </StyleView>
-    </>
+    <View>
+      <Text style={styles.text5xl}>App</Text>
+    </View>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  text5xl: {
     justifyContent: 'center',
+    display: 'flex',
+    flexGrow: 1,
+    alignContent: 'center',
+    fontSize: 50,
+    color: 'red',
+    alignItems: 'center',
   },
 });
